9a06d135400ab95bae62f409a509c6358dd9c5b8
alin.s <alin.s@softisfy.com>
2020-11-27 16:57:23 +0200

    added package

--- lambda/checkURL/checkURL.js
+++ lambda/checkURL/checkURL.js
@@ -1,27 +1,34 @@
 const request = require('request');
 
-const link = "https://nodejs124.org/";
+// const link = "https://nodejs124.org/";
 
-request(link , function (error, response, body) {
-  if(error){
-    console.log('Err: '+ error);
-    return false;
-  }
+// request(link , function (error, response, body) {
+//   if(error){
+//     console.log('Err: '+ error);
+//     return false;
+//   }
 
-  if(response.statusCode == 200 || response.statusCode == 201 || response.statusCode == 202){
-    console.log(link + ' is up!!');
-    return false;
-  }
+//   if(response.statusCode == 200 || response.statusCode == 201 || response.statusCode == 202){
+//     console.log(link + ' is up!!');
+//     return false;
+//   }
 
-  if(response.statusCode == 301 || response.statusCode == 302){
-    console.log(link + ' is redirecting us!!');
-    return false;
-  }
+//   if(response.statusCode == 301 || response.statusCode == 302){
+//     console.log(link + ' is redirecting us!!');
+//     return false;
+//   }
 
-  if(response.statusCode == 401){
-    console.log("you are unauthorized to " + link);
-    return false;
-  }else{
-    console.log(link + ' is down!!');
-  }
-});
+//   if(response.statusCode == 401){
+//     console.log("you are unauthorized to " + link);
+//     return false;
+//   }else{
+//     console.log(link + ' is down!!');
+//   }
+// });
+
+exports.handler = async (event, context, callback) => {
+  callback(null, {
+    statusCode: 200,
+    body: 'Hello World',
+  });
+};
